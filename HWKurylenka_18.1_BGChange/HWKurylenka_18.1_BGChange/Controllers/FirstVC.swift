a1f39cbe28fc055db2f59f86a3f78617571c73a8
Dzmitry <dimchikovich1@gmail.com>
2023-09-12 08:21:09 +0200

    UI complete

---
+++ HWKurylenka_18.1_BGChange/HWKurylenka_18.1_BGChange/Controllers/FirstVC.swift
@@ -0,0 +1,29 @@
+//
+//  FirstVC.swift
+//  HWKurylenka_18.1_BGChange
+//
+//  Created by MACBOOK PRO 15,4 2017 ROK on 12/09/2023.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HWKurylenka_18.1_BGChange/HWKurylenka_18.1_BGChange/Controllers/SecondVC.swift
@@ -0,0 +1,39 @@
+//
+//  SecondVC.swift
+//  HWKurylenka_18.1_BGChange
+//
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
