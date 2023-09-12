605bb28317fdbc677ae71f62042eac4a4e43d985
Dzmitry <dimchikovich1@gmail.com>
2023-09-12 09:36:10 +0200

    Color TF

--- HWKurylenka_18.1_BGChange/HWKurylenka_18.1_BGChange/Controllers/SecondVC.swift
+++ HWKurylenka_18.1_BGChange/HWKurylenka_18.1_BGChange/Controllers/SecondVC.swift
@@ -22,23 +22,23 @@ class SecondVC: UIViewController {
     @IBOutlet weak var colorPreviewView: UIView!
     
     /// Обновление текстовых полей на основе слайдеров
-       @IBAction func redSliderValueChange(_ sender: UISlider) {
-           redTextField.text = String(format: "%.2f", redSlider.value)
-           /// Вызов функции для обновления Hex Color
-           updateHexColor()
-       }
+    @IBAction func redSliderValueChange(_ sender: UISlider) {
+        redTextField.text = String(format: "%.2f", redSlider.value)
+        /// Вызов функции для обновления Hex Color
+        updateHexColor()
+    }
        
-       @IBAction func grenSliderValueChange(_ sender: Any) {
-           greenTextField.text = String(format: "%.2f", greenSlider.value)
-           /// Вызов функции для обновления Hex Color
-           updateHexColor()
-       }
+    @IBAction func grenSliderValueChange(_ sender: Any) {
+        greenTextField.text = String(format: "%.2f", greenSlider.value)
+        /// Вызов функции для обновления Hex Color
+        updateHexColor()
+    }
        
-       @IBAction func blueSliderValueChange(_ sender: Any) {
-           blueTextField.text = String(format: "%.2f", blueSlider.value)
-           /// Вызов функции для обновления Hex Color
-           updateHexColor()
-       }
+    @IBAction func blueSliderValueChange(_ sender: Any) {
+        blueTextField.text = String(format: "%.2f", blueSlider.value)
+        /// Вызов функции для обновления Hex Color
+        updateHexColor()
+    }
        
     @IBAction func opacitySliderValueChange(_ sender: Any) {
         opacityTextField.text = String(format: "%.2f", opacitySlider.value)
@@ -46,6 +46,37 @@ class SecondVC: UIViewController {
         updateHexColor()
     }
 
+    /// Чтобы обеспечить безопасный ввод и избежать сбоев при попытке преобразовать неправильный текст в число, используем Float(sender.text ?? "") для попытки извлечения числа из текстового поля.
+    /// Если ввести недопустимое значение, ничего не произойдет, и текущее значение слайдера не изменится.
+    @IBAction func redTextFieldEditingChanged(_ sender: UITextField) {
+        if let value = Float(sender.text ?? "") {
+            redSlider.value = value
+            updateHexColor()
+        }
+    }
+        
+    @IBAction func greenTextFieldEditingChanged(_ sender: UITextField) {
+        if let value = Float(sender.text ?? "") {
+            greenSlider.value = value
+            updateHexColor()
+        }
+    }
+        
+    @IBAction func blueTextFieldEditingChanged(_ sender: UITextField) {
+        if let value = Float(sender.text ?? "") {
+            blueSlider.value = value
+            updateHexColor()
+        }
+    }
+        
+    @IBAction func opacityTextFieldEditingChanged(_ sender: UITextField) {
+        if let value = Float(sender.text ?? "") {
+            opacitySlider.value = value
+            updateHexColor()
+        }
+    }
+
+    
     func updateHexColor() {
         let red = Int(redSlider.value * 255)
         let green = Int(greenSlider.value * 255)
